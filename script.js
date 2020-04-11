69abf7452972c4806e219bc31c3cbabb8287da39
StineX <stinex.sx@gmail.com>
2020-04-11 16:21:17 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let age = 19;
+
+let answer = prompt("сколько тебе лет?","19");
+console.log (answer);
+
+console.log (age == answer);
+
+let persone = {
+age,
+answer
+}
+console.log (persone);
+
+console.log (persone.age);
+
+let arr = [age, answer];
+console.log (arr);
